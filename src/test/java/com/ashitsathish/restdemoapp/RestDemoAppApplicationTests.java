c5eada96c59562e361c3f42e8fc0e5b4343402f7
asathish <ashitsathish8118@gmail.com>
2020-05-05 20:30:04 -0400

    adding different files of the application to git

---
+++ src/test/java/com/ashitsathish/restdemoapp/RestDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashitsathish.restdemoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestDemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
